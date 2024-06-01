87be21485498e8deb7ec78dd7c358a6e33e5713e
unknown <christianmabeladi@gmail.com>
2024-06-01 20:41:19 +0100

    feat: add authentication for github

---
+++ lib/auth.ts
@@ -0,0 +1,12 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import prisma from "./prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme: {
+    logo: "/logo.png",
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [Github],
+});
