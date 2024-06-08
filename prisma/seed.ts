0ea2e704d56b310edf7fb194a88ad80ddbc9f07d
unknown <christianmabeladi@gmail.com>
2024-06-08 23:45:16 +0100

    create fake post with fakerjs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,11 @@
 import { faker } from "@faker-js/faker";
 import { Prisma, PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-const main = () => {
+const main = async () => {
+  // loop for 10 times
   const users = [];
-
   for (let i = 0; i < 10; i++) {
     const user = {
       username: faker.internet.userName(),
@@ -13,7 +14,50 @@ const main = () => {
       bio: faker.lorem.paragraph(),
       link: faker.internet.url(),
       email: faker.internet.email(),
-    } satisfies Prisma.UserCreateInput;
-    users.push(user);
+    } as const;
+
+    // eslint-disable-next-line no-await-in-loop
+    const dbUser = await prisma.user.create({
+      data: user,
+    });
+
+    users.push(dbUser);
+  }
+
+  const posts = [];
+
+  for (let i = 0; i < 100; i++) {
+    const randomUserIndex = faker.number.int({
+      min: 0,
+      max: users.length - 1,
+    });
+
+    const randomWorldCount = faker.number.int({
+      min: 5,
+      max: 12,
+    });
+    const post = {
+      content: faker.lorem.sentence(randomWorldCount),
+      userId: users[randomUserIndex].id,
+    } as Prisma.PostUncheckedCreateInput;
+
+    // eslint-disable-next-line no-await-in-loop
+
+    const p = await prisma.post.create({ data: post });
+    posts.push(p);
   }
 };
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+
+  .catch(async (e) => {
+    // eslint-disable-next-line no-console
+    console.error(e);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  });
